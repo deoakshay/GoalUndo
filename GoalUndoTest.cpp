ceee228c973f2d4e95a5d20b63dd3b772655e09e
Akshay Deo <akshayd272@gmail.com>
2016-05-17 15:03:30 -0700

    Updated Tests

--- GoalUndoTest.cpp
+++ GoalUndoTest.cpp
@@ -4,7 +4,7 @@
 
 #include <gtest/gtest.h>
 #include "GoalUndo.h"
- 
+
 class GoalUndoTest : public ::testing::Test
 {
 	protected:
@@ -17,4 +17,61 @@ class GoalUndoTest : public ::testing::Test
 TEST(GoalUndoTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+TEST(GoalUndoTest, check_addGoal_basic)
+{
+  GoalUndo gu;
+  gu.addOperation("football","pitch");
+  ASSERT_EQ("football",gu.getGoal());
+  ASSERT_EQ("pitch",gu.getOperations());
+
+}
+
+TEST(GoalUndoTest, check_addOperation_basic)
+{
+  GoalUndo gu;
+  gu.addOperation("halves");
+  ASSERT_EQ("halves",gu.getGoal());
+  ASSERT_EQ("halves",gu.getOperations());
+}
+TEST(GoalUndoTest, check_stackOperations)
+{
+  GoalUndo gu;
+  gu.addOperation("sqaure","horizontalline");
+  gu.addOperation("sqaure","90degrees");
+  gu.addOperation("square","vertical line");
+  gu.undoOperation();
+  ASSERT_EQ("90degrees",gu.getOperations());
+  gu.undoOperation();
+  ASSERT_EQ("horizontalline",gu.getOperations());
+
+}
+TEST(GoalUndoTest, check_emptyStack)
+{
+  GoalUndo gu;
+  ASSERT_EQ("",gu.getOperations());
+}
+
+TEST(GoalUndoTest, check_goalPoppedifnoOperations)
+{
+  GoalUndo gu;
+  gu.addOperation("sqaure","horizontalline");
+  gu.addOperation("sqaure","90degrees");
+  gu.addOperation("square","vertical line");
+  gu.undoOperation();
+  gu.undoOperation();
+  gu.undoOperation();
+  ASSERT_EQ("",gu.getGoal());
+  gu.undoOperation();
+  ASSERT_EQ("",gu.getGoal());
+}
+
+TEST(GoalUndoTest, check_ifAllOperationsReturned)
+{
+  GoalUndo gu;
+  gu.addOperation("sqaure","horizontalline");
+  gu.addOperation("sqaure","90degrees");
+  gu.addOperation("square","verticalline");
+  ASSERT_EQ("horizontalline 90degrees verticalline",gu.getOperations());
 }
